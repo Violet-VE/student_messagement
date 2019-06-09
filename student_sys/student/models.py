23ed08cfe3109ef765decaf77170a31808630228
violet-ve <m.sakura.saber@outlook.com>
2019-06-09 15:58:36 +0800

    Django_Exercise_Finished

---
+++ student_sys/student/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    SEX_ITEMS = [
+        (1, '男'),
+        (2, '女'),
+        (0, '扶他'),
+    ]
+    STATUS_ITEMS = [
+        (0, '申请中'),
+        (1, '通过'),
+        (2, '拒绝'),
+    ]
+    name = models.CharField(verbose_name="姓名", max_length=128)
+    sex = models.IntegerField(verbose_name="性别", choices=SEX_ITEMS)
+    profession = models.CharField(verbose_name="职业", max_length=128)
+    email = models.EmailField(verbose_name="Email", max_length=254)
+    qq = models.CharField(verbose_name="QQ", max_length=128)
+    phone = models.CharField(verbose_name="电话", max_length=128)
+
+    status = models.IntegerField(
+        verbose_name="审核状态", choices=STATUS_ITEMS, default=0)
+    created_time = models.DateTimeField(
+        verbose_name="创建时间", editable=False, auto_now_add=True)
+
+    def __str__(self):
+        return '<Student: {}>'.format(self.name)
+
+    @classmethod
+    def get_all(cls):
+        return cls.objects.all()
+
+    class Meta:
+        verbose_name = verbose_name_plural = "学员信息"
---
+++ student_sys/student/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import View
+
+# Create your views here.
+
+from .models import Student
+from .forms import StudentForm
+
+
+class IndexView(View):
+    # Class-based view:
+    template_name = 'index.html'
+
+    def get_context(self):
+        students = Student.get_all()
+        context = {
+            'students': students,
+        }
+        return context
+
+    def get(self, request):
+        context = self.get_context()
+        form = StudentForm()
+        context.update({
+            'form': form,
+        })
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+        context = self.get_context()
+        context.update({
+            'form': form,
+        })
+        return render(request, self.template_name, context=context)
+
+    # 流水式代码：
+    # def index(request):
+    #   students = Student.get_all()
+    #   if request.method == 'POST':
+    #       form = StudentForm(request.POST)
+    #       if form.is_valid():
+    #           form.save()
+    #           return HttpResponseRedirect(reverse('index'))
+    #   else:
+    #       form = StudentForm()
+    #   context = {
+    #       'students': students,
+    #       'form': form,
+    #   }
+    #   return render(request, 'index.html', context=context)
+
+    # students = Student.objects.all()
+    # return render(request, 'index.html', context={'students': students})
+    # words = 'World!'
+    # return render(request, 'index.html', context={'words': words})
+
+    # 手动构建Form
+    # students = Student.objects.all()
+    # if request.method == 'POST':
+    #     form = StudentForm(request.POST)
+    #     if (form.is_valid()):
+    #         cleaded_data = form.cleaned_data
+    #         student = Student()
+    #         student.name = cleaded_data['name']
+    #         student.name = cleaded_data['sex']
+    #         student.name = cleaded_data['profession']
+    #         student.name = cleaded_data['email']
+    #         student.name = cleaded_data['qq']
+    #         student.name = cleaded_data['phone']
+    #         student.save()
+    #         return HttpResponseRedirect(reverse('index'))
+    # else:
+    #     form = StudentForm()
+    # context = {
+    #     'students': students,
+    #     'form': form,
+    # }
+    # return render(request, 'index.html', context=context)
